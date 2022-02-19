105e11e6b37daab3b5dcdf2688857be9d3664dce
JoJa <joeds133@hotmail.com>
2022-02-19 23:15:14 +0700

    m/stokm/hr

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -4,7 +4,12 @@ import { DataGrid } from '@mui/x-data-grid'
 const DataTable = ({ data }) => {
   const columns = [
     { field: 'LaneNo', headerName: 'Lane No.', width: 100, flex: 1 },
-    { field: 'velocity', headerName: 'Velocity [ m/s ]', width: 200, flex: 1 },
+    {
+      field: 'velocity',
+      headerName: 'Velocity [ km/hr ]',
+      width: 200,
+      flex: 1,
+    },
     { field: 'time', headerName: 'Time [ hh:mm:ss ]', width: 100, flex: 1 },
   ]
   return (
